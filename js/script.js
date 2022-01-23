576013b997ecf10ec80dcdaaca8c2397c819d0ba
Sakib-Dev1 <dev.secondary1.acc@gmail.com>
2022-01-23 17:51:28 +0600

    Bismillah

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
+// setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+// setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
-
+// show nav
 navBtn.addEventListener("click", () => {
+  console.log("button click");
   navbar.classList.add("showNav");
 });
-
-navClose.addEventListener("click", () => {
+// close nav
+navBtn.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
